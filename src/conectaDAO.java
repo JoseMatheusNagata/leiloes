ad2d5897e57e918e6a67883d82ec3bc25db8344c
heygan <matheusjapa2010@hotmail.com>
2023-07-16 12:41:44 -0300

    criado metodo para cadastrar produto e ajudado conexao com banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,59 @@
 
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
+import java.sql.ResultSet;
 
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/leiloes user=root &password=H7zvsi36!@#$");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
+
+    Connection conn;
+    PreparedStatement st;
+    ResultSet rs;
+
+    public Connection getConexao() {
+
         return conn;
     }
-    
+
+    public boolean connectDB() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/leiloes", "root", "H7zvsi36!@#$");
+
+            return true;
+
+        } catch (SQLException | ClassNotFoundException ex) {
+
+            System.out.println("Erro ConectaDAO " + ex.getMessage());
+
+            return false;
+        }
+
+    }
+
+    public void desconectarDB() {
+
+        try {
+
+            conn.close();
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
 }
